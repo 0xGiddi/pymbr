1597fab1d7d62742204839367f0faf09820a36fa
0xGiddi <hello@giddi.net>
2017-06-05 23:12:37 +0300

    Added __ini__ comten. First working version

---
+++ pymbr/__init__.py
@@ -0,0 +1,12 @@
+"""
+    pymbr
+    
+    A python module to manipulate and create MBRs.
+    :copyright: (c) 2017 by Gideon S. (0xGiddi)
+    :license: GPLv3, see LICENSE file for more details.
+"""
+
+__version__ = "1.0.0"
+
+from .bootrecord import *
+from .partition import *
